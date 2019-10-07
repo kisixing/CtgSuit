48101e08ed56ed2a50dcd1f056d464c7a14c0363
lxf <lixiaofan.dev@gmail.com>
2019-10-07 21:36:33 +0800

    style: blue theme

--- src/pages/Workbench/Item.tsx
+++ src/pages/Workbench/Item.tsx
@@ -30,6 +30,7 @@ const WorkbenchItem = props => {
           icon="fullscreen"
           size="small"
           type="link"
+          style={{color:"#fff"}}
           onClick={fullScreen.bind(this)}
         ></Button>
       </div>
@@ -77,6 +78,7 @@ const WorkbenchItem = props => {
         size="small"
         className={styles.card}
         extra={renderExtra(dataSource.status)}
+        headStyle={{background:'#004c8c',color:'#fff'}}
         bodyStyle={{ padding: 0, height: 'calc(100% - 40px)' }}
       >
         <L data={data} mutableSuitObject={suitObject} itemHeight={itemHeight}></L>
