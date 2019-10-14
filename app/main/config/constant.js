6edc0d7a0c47786fd20e4bf5cac5660b3c740a46
lxf <lixiaofan.dev@gmail.com>
2019-10-14 18:41:25 +0800

    refactor: printer

--- app/main/config/constant.js
+++ app/main/config/constant.js
@@ -2,5 +2,6 @@ const path = require('path');
 
 module.exports = {
     SETTING_PATH: '.setting',
-    DEFAULT_SETTING_PATH:path.resolve(__dirname,'.defaultSetting')
+    DEFAULT_SETTING_PATH: path.resolve(__dirname, '.defaultSetting'),
+    PRINTER_PATH: path.resolve(__dirname, '../libs/PDFtoPrinterSelect.exe')
 }
