d42e7cb67aa1b57fe97b6b0ae5ac7f228373e1f2
lxf <lixiaofan.dev@gmail.com>
2019-11-15 15:56:33 +0800

    download optimization

--- app/main/ipc/main.js
+++ app/main/ipc/main.js
@@ -8,25 +8,21 @@ module.exports = () => {
     ipcMain.on('\u0064\u006f\u0077\u006e\u006c\u006f\u0061\u0064\u0041\u0070\u0070', appDownload)
 }
 function appDownload(e, url = '') {
-    const { pathname, hostname, port, href } = parse(url)
-    console.log(1111, href)
-
+    const { pathname } = parse(url)
     const filename = pathname.slice(pathname.lastIndexOf('/') + 1)
     const insDir = resolve(process.env.USERPROFILE, 'Desktop')
     const filePath = resolve(insDir, filename)
 
     existsSync(insDir) || mkdirSync(insDir)
 
-
-    const r = request({ hostname, port, pathname }, res => {
-        res.pipe(createWriteStream(filePath)).on('close', () => {
+    const writeStream = createWriteStream(filePath)
+        .on('close', () => {
             e.sender.send('\u0061\u0070\u0070\u0044\u006f\u0077\u006e\u006c\u006f\u0061\u0064\u0065\u0064')
             dialog.showMessageBox({
-                message: '\u4e0b\u8f7d\u5b8c\u6210\uff0c\u662f\u5426\u8fdb\u884c\u5b89\u88c5\uff1f',
+                message: '下载完成，是否进行安装？',
                 buttons: ['cancel', 'ok'],
-            }, resNum => exec(resNum ? filePath : `explorer ${insDir}`))
+            }, _ => exec(`explorer ${_ ? filePath : insDir}`))
         })
-    })
-    r.end()
+    request(url, incomingMessage => incomingMessage.pipe(writeStream)).end()
 
 }
