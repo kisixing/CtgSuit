f6c4225caf9d09186dbe656a8d14acdcd1c99d10
zwj7181 <45657181@qq.com>
2019-12-02 18:41:27 +0800

    调整建档调用功能

--- src/pages/Workbench/CollectionCreateForm.tsx
+++ src/pages/Workbench/CollectionCreateForm.tsx
@@ -2,13 +2,38 @@
  * 胎监主页PDA建档/绑定弹窗
  */
 import React, { useState } from 'react';
-import { Button, Modal, Form, Input, Row, Col, InputNumber, message } from 'antd';
+import { Button, Modal, Form, Input, Row, Col, InputNumber, message, Table } from 'antd';
 import { WrappedFormUtils } from 'antd/lib/form/Form';
 import moment from 'moment';
 import { request } from '@lianmed/utils';
 import { stringify } from 'qs';
+import store from '@/utils/SettingStore';
 
 const width = '200px';
+const columns = [
+  {
+    title: '床号',
+    dataIndex: 'bedNO',
+    key: 'bedNO',
+    align: 'center',
+    width: '33.33%',
+  },
+  {
+    title: '姓名',
+    dataIndex: 'name',
+    key: 'name',
+    align: 'center',
+    width: '33.33%',
+  },
+  {
+    title: '住院号',
+    dataIndex: 'inpatientNO',
+    key: 'inpatientNO',
+    align: 'center',
+    width: '33.33%',
+  },
+];
+
 interface IProps {
   form: WrappedFormUtils
   starttime: string
@@ -24,10 +49,15 @@ const CollectionCreateForm = (props: IProps) => {
   const { starttime, docid, isTodo, bedname, visible, onCancel, form, onCreated } = props
 
   const { getFieldDecorator } = form;
-
+  const [areaNO, setAreaNO] = useState('');
+  // 搜索值
+  const [pregnancyList, setPregnancyList] = useState([]);
   const [disabled, setDisabled] = useState(false)
   const [errorText, setErrorText] = useState('')
   const [loading, setLoading] = useState(false)
+
+  // 获取病区好
+  store.getObj().then(e => setAreaNO(e.areaNO));
 
   const reset = () => {
     // 清空form表单数据、输入框状态变为可输入状态
@@ -66,11 +96,15 @@ const CollectionCreateForm = (props: IProps) => {
     } else {
       // 无孕册pregnancyId 新建孕册获并获取到pregnancyId
       // 新建孕册
-      const res = await request.post('/pregnancies', { data: { ...values, areaNO: '01', recordstate: '10' } }).catch(({ data }) => {
-        data.then(({ title }) => {
-          message.info(title);
+      const res = await request
+        .post('/pregnancies', {
+          data: { ...values, areaNO: areaNO, recordstate: '10' },
         })
-      })
+        .catch(({ data }) => {
+          data.then(({ title }) => {
+            message.info(title);
+          });
+        });
       if (res && res.id) {
         // message.success('孕册创建成功！');
         if (res && res.id) {
@@ -97,7 +131,7 @@ const CollectionCreateForm = (props: IProps) => {
         message.success('建档成功！')
         onCreated(res)
         setLoading(false)
-        onCancel();
+        handleCancel();
       }, 3000);
     } else {
       message.error('建档异常，请稍后再试！', 3);
@@ -111,28 +145,42 @@ const CollectionCreateForm = (props: IProps) => {
       if (err) {
         return;
       }
-      const res = await request.get(`/pregnanciespage?${stringify({
-        // TODO
-        // 默认病区 、默认住院状态
-        'recordstate.equals': '10', // 住院中
-        'areaNO.equals': '01', // 默认病区
-        'bedNO.equals': values.bedNO, // 床号
-      })}`)
+      const res = await request.get(
+        `/pregnanciespage?${stringify({
+          // TODO
+          // 默认病区 、默认住院状态
+          'recordstate.equals': '10', // 住院中
+          'areaNO.equals': areaNO, // 默认病区
+          'bedNO.equals': values.bedNO ? values.bedNO : undefined, // 床号
+          'inpatientNO.equals': values.inpatientNO ? values.inpatientNO : undefined, // 住院号
+          'name.equals': values.name ? values.name : undefined,
+        })}`,
+      );
       // 成功调入孕妇信息后，禁止修改。
       // 重新选择调入、新建孕妇信息 --> '取消'后再重复操作
       if (!res.length) {
         setErrorText('没有这个孕册，请新建孕册。')
-      } else {
-        setDisabled(true)
+      } else if (res.length === 1) {
+        // 搜索结果只有一个，默认赋值
+        setDisabled(true);
         form.setFieldsValue(res[0]);
+      } else {
+        setPregnancyList(res);
+        // setDisabled(true)
+        // form.setFieldsValue(res[0]); // TODO 12.02前的方案，自动赋值
       }
     });
+  };
+
+  const selectRow = record => {
+     form.setFieldsValue(record);
+     setDisabled(true);
   };
 
   const handleCreate = () => {
     form.validateFields((err, values) => {
       if (err) {
-        return 0
+        return null;
       } else {
         if (!values.bedNO) {
           return message.error('请输入患者床号！');
@@ -151,6 +199,7 @@ const CollectionCreateForm = (props: IProps) => {
   // 关闭窗口是，重置disabled
   const handleCancel = () => {
     setDisabled(false);
+    setPregnancyList([]);
     onCancel();
   }
 
@@ -226,7 +275,7 @@ const CollectionCreateForm = (props: IProps) => {
   const formItemLayout = {
     labelCol: {
       xs: { span: 24 },
-      sm: { span: 8 },
+      sm: { span: 6 },
     },
     wrapperCol: {
       xs: { span: 24 },
@@ -245,7 +294,7 @@ const CollectionCreateForm = (props: IProps) => {
       footer={null}
       okText="创建"
       cancelText="取消"
-      bodyStyle={{ paddingRight: '48px' }}
+      bodyStyle={{}}
       onCancel={handleCancel}
     >
       <Form id="Modal_Message_Container" layout="horizontal" {...formItemLayout}>
@@ -258,10 +307,10 @@ const CollectionCreateForm = (props: IProps) => {
             </Form.Item>
           </Col>
           <Col span={12}>
-            <Form.Item label="床号">
+            <Form.Item label={<span className="required">床号</span>}>
               {getFieldDecorator('bedNO', {
                 rules: [
-                  { required: true, message: '请填写孕妇床号!' },
+                  { required: false, message: '请填写孕妇床号!' },
                   { max: 6, message: '床号的最大长度为6' },
                   { validator: validateNoChinese },
                 ],
@@ -354,18 +403,14 @@ const CollectionCreateForm = (props: IProps) => {
             </Col> */}
         </Row>
         <Row>
-          <Col span={24} style={{ marginBottom: 24, textAlign: 'center' }}>
+          <Col span={24} style={{ marginBottom: 12, textAlign: 'center' }}>
             {/* 清空form数据 */}
             <Button onClick={reset}>重置</Button>
             {/* 建档后，不支持再次修改信息 */}
             <Button style={{ margin: '0 20px' }} type="primary" onClick={handleSearch}>
               调入
-              </Button>
-            <Button
-              type="primary"
-              onClick={handleCreate}
-              loading={loading}
-            >
+            </Button>
+            <Button type="primary" onClick={handleCreate} loading={loading}>
               确认
             </Button>
           </Col>
@@ -373,6 +418,7 @@ const CollectionCreateForm = (props: IProps) => {
             span={24}
             style={{
               position: 'relative',
+              marginBottom: '12px',
               color: '#999',
               textAlign: 'center',
             }}
@@ -391,6 +437,24 @@ const CollectionCreateForm = (props: IProps) => {
           </Col>
         </Row>
       </Form>
+      {pregnancyList.length > 1 && (
+        <div>
+          {/* <p>请选择孕册信息</p> */}
+          <Table
+            bordered
+            size="small"
+            scroll={{ y: 228 }}
+            pagination={false}
+            columns={columns}
+            dataSource={pregnancyList}
+            onRow={record => {
+              return {
+                onClick: event => selectRow(record), // 点击行
+              };
+            }}
+          />
+        </div>
+      )}
     </Modal>
   );
 }
