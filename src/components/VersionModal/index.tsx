62e93819919af425a4696a6f1714402c688aae6e
lxf <lixiaofan.dev@gmail.com>
2019-12-20 20:53:59 +0800

    version manager

--- src/components/VersionModal/index.tsx
+++ src/components/VersionModal/index.tsx
@@ -17,31 +17,33 @@ export const SignModal = (props: IProps) => {
 
     const target = __VERSION_MANIFEST.find(_ => _.version === __VERSION)
     return (
-        <Modal
-            maskClosable={false}
-            destroyOnClose
-            visible={visible}
-            title={`${__VERSION}版本更新说明`}
-            cancelText={null}
-            onCancel={() => {
-                setVisible(false)
-                localStorage.setItem('version', __VERSION)
-            }}
-            onOk={() => {
-                setVisible(false)
-                localStorage.setItem('version', __VERSION)
-            }}
-        >
-            <ol>
-                {
-                    target && target.details.map((_, i) => {
-                        return <li>{i + 1}、{_}</li>
-                    })
+        target && target.details && (
+            <Modal
+                maskClosable={false}
+                destroyOnClose
+                visible={visible}
+                title={`${__VERSION}版本更新说明`}
+                cancelText={null}
+                onCancel={() => {
+                    setVisible(false)
+                    localStorage.setItem('version', __VERSION)
+                }}
+                onOk={() => {
+                    setVisible(false)
+                    localStorage.setItem('version', __VERSION)
+                }}
+            >
+                <ol>
+                    {
+                        target && target.details.map((_, i) => {
+                            return <li>{i + 1}、{_}</li>
+                        })
 
-                }
-            </ol>
+                    }
+                </ol>
 
-        </Modal>
+            </Modal>
+        )
     );
 }
 
