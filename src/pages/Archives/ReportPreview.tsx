0c42b3a148e5568eabb109c398a99bbd068a3668
zwj7181 <45657181@qq.com>
2020-01-05 21:02:38 +0800

    查看报告有多个时，增加选择功能

--- src/pages/Archives/ReportPreview.tsx
+++ src/pages/Archives/ReportPreview.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState, useLayoutEffect, useRef } from 'react';
+import { Menu } from 'antd';
 import PreviewContent from "@lianmed/pages/lib/Ctg/Report/PreviewContent";
 import Shell from "../Workbench/Analysis/Shell";
 import { request } from "@lianmed/utils";
+
 interface IProps {
   report: string
   docid: string
@@ -14,34 +16,90 @@ interface IProps {
   gestationalWeek: string
 }
 export const Context = React.createContext({});
-function Analysis(props: IProps) {
-  const { report = '' } = props
+function ReportPreview(props: IProps) {
+  const { report = '' } = props;
+  let reportObj = {};
+  try {
+    reportObj = JSON.parse(report);
+  } catch (error) {
+    console.log('report格式不正确', error);
+    return null;
+  }
 
-  const [pdfBase64, setPdfBase64] = useState('')
-  const inputEl = useRef(null)
-  const [wh, setWh] = useState({ w: 0, h: 0 })
+  // 是否为对象
+  const isObj = Object.prototype.toString.call(reportObj);
+  if (!isObj) {
+    return null;
+  }
+  const arr = [];
+  for (let key in reportObj) {
+    const obj = { key, value: reportObj[key] };
+    arr.push(obj);
+  }
+
+  const [currentReport, setCurrentReport] = useState(arr[0]['key']);
+  const [pdfBase64, setPdfBase64] = useState('');
+  const inputEl = useRef(null);
+  const [wh, setWh] = useState({ w: 0, h: 0 });
   useLayoutEffect(() => {
     const { clientHeight, clientWidth } = inputEl.current;
-    setWh({ h: clientHeight, w: clientWidth })
-  }, [])
+    setWh({ h: clientHeight, w: clientWidth - 176 });
+  }, []);
 
   useEffect(() => {
-    request.get('/ctg-exams-pdf', {
-      params: {
-        report
-      }
-    }).then(({ pdfdata }) => {
-      pdfdata && setPdfBase64(`data:application/pdf;base64,${pdfdata}`)
-    })
-  }, [])
+    fetchpdf(currentReport);
+  }, []);
+
+  const fetchpdf = (value: string) => {
+    request
+      .get('/ctg-exams-pdf', {
+        params: {
+          report: value,
+        },
+      })
+      .then(({ pdfdata }) => {
+        pdfdata && setPdfBase64(`data:application/pdf;base64,${pdfdata}`);
+      });
+  };
+
+  const handleClick = ({ key }) => {
+    setCurrentReport(key);
+    fetchpdf(key);
+  };
 
   return (
     <Shell {...props}>
-      <div style={{ height: '100%', textAlign: 'center', display: 'flex' }} ref={inputEl}>
-        <PreviewContent pdfBase64={pdfBase64} wh={wh} isFull borderd={false} />
+      <div
+        style={{ height: '100%', textAlign: 'center', display: 'flex' }}
+        ref={inputEl}
+      >
+        <Menu
+          style={{ width: 176 }}
+          defaultSelectedKeys={[currentReport]}
+          theme="light"
+          onClick={handleClick}
+        >
+          {arr &&
+            arr.map(e => {
+              return (
+                <Menu.Item key={e.key}>
+                  <div>{e.key}</div>
+                  <div>{e.value}</div>
+                </Menu.Item>
+              );
+            })}
+        </Menu>
+        <div style={{ flex: 1 }}>
+          <PreviewContent
+            pdfBase64={pdfBase64}
+            wh={wh}
+            isFull
+            borderd={false}
+          />
+        </div>
       </div>
     </Shell>
   );
 }
 
-export default Analysis;
+export default ReportPreview;
