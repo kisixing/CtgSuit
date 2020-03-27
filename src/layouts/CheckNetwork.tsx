2fcd9f10f4b2ffe05e72045e73a56c0089638c79
lxf <lixiaofan.dev@gmail.com>
2020-03-27 14:31:59 +0800

    style

--- src/layouts/CheckNetwork.tsx
+++ src/layouts/CheckNetwork.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useCheckNetwork } from "@lianmed/lmg";
 import { connect } from 'dva';
+import { InfoCircleOutlined } from '@ant-design/icons';
 
 function CheckNetwork(props) {
     const { dispatch, isOn } = props
@@ -39,7 +40,8 @@ function CheckNetwork(props) {
                     overflow: 'hidden',
                     borderRadius: small && 2
                 }}>
-                <span style={{ color: '#fff', }}>网络不可用，请检查你的网络设置（点击刷新）</span>
+                <InfoCircleOutlined />
+                <span style={{ color: '#fff',margin:'0 6px' }}>网络不可用，请检查你的网络设置</span>
                 {/* <Button size={small ? 'small' : 'default'} icon="reload" type="link" style={{ color: '#fff' }} onClickCapture={e => location.reload()} /> */}
                 {/* <Button size={small ? 'small' : 'default'} icon={small ? 'column-height' : 'minus'} type="link" style={{ color: '#fff', marginLeft: 'auto' }} onClickCapture={e => setSmall(!small)} /> */}
 
